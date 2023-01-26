7d357cc91265b9c99b908c7dd59534ec2b199973
Kenny Li <112835000+kcl20@users.noreply.github.com>
2023-01-26 14:41:41 -0500

    Added delete comment method and event listener..

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,10 +1,14 @@
+const blog_url = window.location.href;
+  let urlParts = blog_url.split('/');
+  let blog_id = urlParts[urlParts.length - 1];
+
 const newCommentHandler = async (event) => {
   event.preventDefault();
   const description = document.querySelector('#blog-comment').value.trim();
 
-  const blog_url = window.location.href;
-  let urlParts = blog_url.split('/');
-  let blog_id = urlParts[urlParts.length - 1];
+  // const blog_url = window.location.href;
+  // let urlParts = blog_url.split('/');
+  // let blog_id = urlParts[urlParts.length - 1];
   console.log("button pressed");
   // console.log("url" + blog_url);
   // console.log("url parts " + urlParts);
@@ -27,6 +31,33 @@ const newCommentHandler = async (event) => {
       }
     }
 }
+
+const delButtonHandler = async (event) => {
+  console.log("delete button pressed");
+  const delete_comment_id = event.target.getAttribute('data-id');
+  console.log("delete comment id: " + delete_comment_id);
+
+  if (delete_comment_id) {
+    
+
+    const response = await fetch(`/api/comments/${delete_comment_id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      console.log(response);
+      document.location.replace(`/blog/${blog_id}`);
+    } else {
+      alert('Failed to delete comment');
+    }
+  }
+};
+
+
+document
+  .querySelector('.comment-list')
+  .addEventListener('click', delButtonHandler);
+
 
 
 document
