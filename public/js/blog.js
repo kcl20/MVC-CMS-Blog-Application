0c22a2965faa21f47b5256b9d4108a79c9246319
Kenny Li <112835000+kcl20@users.noreply.github.com>
2023-01-26 11:03:02 -0500

    Update blog.js

--- public/js/blog.js
+++ public/js/blog.js
@@ -6,9 +6,9 @@ const newCommentHandler = async (event) => {
   let urlParts = blog_url.split('/');
   let blog_id = urlParts[urlParts.length - 1];
   console.log("button pressed");
-  console.log("url" + blog_url);
-  console.log("url parts " + urlParts);
-  console.log("blog id " + blog_id);
+  // console.log("url" + blog_url);
+  // console.log("url parts " + urlParts);
+  console.log("blog id: " + blog_id);
   console.log("comment: " + description);
 
   if (blog_id && description) {
@@ -21,7 +21,7 @@ const newCommentHandler = async (event) => {
       });
       if (response.ok) {
         // document.location.replace('/blog/${blog_id}');
-        console.log("Added comment: " + description);
+        console.log("Added comment.");
       } else {
         alert('Failed to create comment');
       }
