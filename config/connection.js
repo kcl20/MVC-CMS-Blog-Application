fedbf98f5f83b5c2a1c76cbca1640a3565ed3358
Kenny Li <112835000+kcl20@users.noreply.github.com>
2023-01-26 22:41:30 -0500

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,44 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// original
+// let sequelize;
+
+// if (process.env.BLOGDB_URL) {
+//   sequelize = new Sequelize(process.env.BLOGDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306 // do not change this, it is the default port for MySQL
+//     }
+//   );
+// }
+
+
+//from coding-boot-camp
 let sequelize;
 
-if (process.env.BLOGDB_URL) {
-  sequelize = new Sequelize(process.env.BLOGDB_URL);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306 // do not change this, it is the default port for MySQL
-    }
+      port: 3306,
+    },
   );
 }
+
+
 
 // const Sequelize = require('sequelize');
 // sequelize = new Sequelize(process.env.DATABASE_URL, {
